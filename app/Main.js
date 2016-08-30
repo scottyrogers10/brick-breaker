72c14f38a4d2c4e09c90e51324349468924fa6e4
Scotty Rogers <scotty.rogers10@gmail.com>
2016-08-29 23:21:56 -0600

    Total redo... created the axis-js framework and am rebuilding the game using that.

--- app/Main.js
+++ app/Main.js
@@ -1,40 +1,52 @@
 require([
-    "app/src/entities/Entities.js"
-], function (entities) {
-    var brickBreaker = null;
-
+    "lib/axis-js/Axis.js"
+], function (Axis) {
     var init = function () {
-        brickBreaker = new Foundation.Game({
-            type: "2d",
-            width: "640",
-            height: "480"
+        var world = new Axis.World();
+        world.rootEntity = new Axis.Entity("rootEntity");
+        world.width = 1000;
+        world.height = 1000;
+
+        var brickBreaker = new Axis.Game(world);
+
+        var mainViewport = new Axis.Viewport("main-viewport", 600, 400);
+        mainViewport.active = true;
+
+        brickBreaker.addViewport(mainViewport);
+
+        //TODO: Handle SpriteSheet images better then this!
+        var spriteSheet = new Image();
+        spriteSheet.src = "app/img/spritesheet.png";
+
+        spriteSheet.addEventListener("load", function () {
+            var paddle = new Axis.Entity("paddle");
+
+            var paddlePosition = new Axis.Component.Position();
+            paddlePosition.x = 800;
+            paddlePosition.y = 800;
+            paddle.addComponent(paddlePosition);
+
+            var paddleSize = new Axis.Component.Size();
+            paddleSize.w = 104;
+            paddleSize.h = 24;
+            paddle.addComponent(paddleSize);
+
+            var paddleSprite = new Axis.Component.Sprite();
+            paddleSprite.img = this;
+            paddleSprite.srcX = 38;
+            paddleSprite.srcY = 0;
+            paddleSprite.srcW = 104;
+            paddleSprite.srcH = 24;
+            paddle.addComponent(paddleSprite);
+
+            brickBreaker.world.rootEntity.addChildEntity(paddle);
+
+            brickBreaker.addSystem(new Axis.System.RenderSystem());
+
+            brickBreaker.runGameLoop();
         });
 
-        var canvas = new Foundation.View(brickBreaker, "main-canvas");
-        brickBreaker.add.view(canvas);
-
-        var spriteSheet = new Foundation.SpriteSheet("app/img/spritesheet.png");
-        brickBreaker.add.spriteSheet(spriteSheet);
-
-        var prepareGameObjects = function () {
-            brickBreaker.add.system(new Foundation.System.DPad(brickBreaker));
-            brickBreaker.add.system(new Foundation.System.State(brickBreaker));
-            brickBreaker.add.system(new Foundation.System.Collision(brickBreaker));
-            brickBreaker.add.system(new Foundation.System.Restraint(brickBreaker));
-            brickBreaker.add.system(new Foundation.System.Render(brickBreaker, "main-canvas"));
-
-            entities.init(brickBreaker);
-
-            run();
-        };
-
-        brickBreaker.spriteSheet.addEventListener("load", function () {
-            prepareGameObjects();
-        });
-    };
-
-    function run () {
-        brickBreaker.runGameLoop();
+        window.brickBreaker = brickBreaker;
     };
 
     init();
